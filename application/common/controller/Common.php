9b08828c6f1cb0b70ee21c34a95f5ca5c62f7087
xank <xank@qq.com>
2017-11-09 07:53:36 +0800

    支持跨域

--- application/common/controller/Common.php
+++ application/common/controller/Common.php
@@ -17,9 +17,12 @@ class Common extends Controller
     {
         parent::_initialize();
         /*防止跨域*/      
-        header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
+        header('Access-Control-Allow-Origin: *');
+        // header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
         header('Access-Control-Allow-Credentials: true');
         header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+
+        
         header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, authKey, sessionId");
         $param =  Request::instance()->param();            
         $this->param = $param;
