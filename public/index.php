f5b11974c05d2795ca710f40b63adbe9a97ced21
xank <xank@qq.com>
2017-11-08 20:46:40 +0800

    vuethink

--- public/index.php
+++ public/index.php
@@ -11,7 +11,9 @@
 
 // [ 应用入口文件 ]
 
-// 定义应用目录
+// 应用目录
 define('APP_PATH', __DIR__ . '/../application/');
+// 定义配置文件目录和应用目录同级
+define('CONF_PATH', __DIR__.'/../config/');
 // 加载框架引导文件
 require __DIR__ . '/../thinkphp/start.php';
--- public/router.php
+++ public/router.php
@@ -9,12 +9,8 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 // $Id$
-
-if (is_file($_SERVER["DOCUMENT_ROOT"] . $_SERVER["SCRIPT_NAME"])) {
+if (is_file($_SERVER["DOCUMENT_ROOT"] . $_SERVER["REQUEST_URI"])) {
     return false;
 } else {
-    if (!isset($_SERVER['PATH_INFO'])) {
-        $_SERVER['PATH_INFO'] = $_SERVER['REQUEST_URI'];
-    }
     require __DIR__ . "/index.php";
 }
